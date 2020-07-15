0207e72efab3bf4c9609d9918352922f333f4705
eruong <ethan.ong@pomona.edu>
2020-07-14 20:29:02 -0700

    ready for Heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 # Authors: CS-World Domination Summer19 - JG
 try:
     from flask import Flask
+    import gunicorn
 except:
     print('Make sure to pip install Flask twilio')
 from config import Config
